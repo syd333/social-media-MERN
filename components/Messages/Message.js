d90a961da50821937fb878dfb8735d3063aaea16
syd333 <sydmiranda@icloud.com>
2022-06-15 14:46:08 -0400

    banner layout

---
+++ components/Messages/Message.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { Icon, Popup } from "semantic-ui-react";
+import calculateTime from "../../utils/calculateTime";
+
+
+function Message () {
+
+    return (
+        <div>
+            message
+            </div>
+    )
+}
+
+export default Message;
---
+++ components/Messages/MessageInputField.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { Form, Segment } from "semantic-ui-react";
+
+function MessageInputField() {
+  return;
+  <div>message field input</div>;
+}
+
+export default MessageFieldInput;
